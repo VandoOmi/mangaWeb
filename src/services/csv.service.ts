bcc7d3f485d14e2800bfe7979a2b8d2629cc0095
vando02800 <vando.02800@gmail.com>
2024-12-04 08:26:46 +0100

    set up

---
+++ src/services/csv.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable
+({
+  providedIn: 'root'
+})
+export class CsvService
+{
+
+  private csvPath!: URL;
+  private content!: string;
+
+  constructor() { }
+
+  getContent(): string 
+  {
+    return this.content;
+  }
+
+  async init(path: string) 
+  {
+    this.csvPath = new URL(path);
+    this.content =  await this.load();
+  }
+
+  private async load(): Promise<string>
+  {
+    try
+    {
+      const response = await fetch(this.csvPath);
+      if(!response.ok)
+      {
+        throw new Error(`Fehler beim Laden der Datei: ${response.statusText}`)
+      }
+      return await response.text();
+    }
+    catch(error) 
+    {
+      console.error("Fehler beim Laden der CSV-Datei:", error);
+      return '';
+    }
+  } 
+}
---
+++ src/services/user.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { CsvService } from './csv.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  static userCsv = 'src/csv/user.csv';
+  content: string;
+
+  constructor(csvServ: CsvService)
+  { 
+    csvServ.init(UserService.userCsv);
+    this.content = csvServ.getContent();
+  }
+
+  async readUserList(): Promise<Array<User>> {
+    const lines = this.content.split("\n");
+    let userList: Array<User> = new Array<User>;
+
+    lines.forEach(e => {
+      const line = e.trim().split(";");
+      if(line.length === 4) {
+      userList.push(new User(line[0],line[1],line[2],line[3]))
+      }
+    });
+
+    return userList;
+  }
+}
+
+export class User {
+  private id:string;
+  private name:string;
+  private password:string;
+  private role:string;
+
+  constructor
+  (
+    id:string,
+    name:string,
+    password:string,
+    role:string
+  )
+  {
+    this.id = id;
+    this.name = name;
+    this.password = password;
+    this.role = role;
+
+    
+  }
+}
