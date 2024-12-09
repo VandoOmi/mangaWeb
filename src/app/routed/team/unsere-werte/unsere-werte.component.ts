9a31378e8c4c2d3b0d1de2ea25a96b2b434f854e
fabi7798 <hoffmann.fabian16@gmail.com>
2024-12-09 13:19:04 +0100

    teams

---
+++ src/app/routed/team/unsere-werte/unsere-werte.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unsere-werte',
+  standalone: true,
+  imports: [],
+  templateUrl: './unsere-werte.component.html',
+  styleUrl: './unsere-werte.component.scss'
+})
+export class UnsereWerteComponent {
+
+}
