57fe8a854bfd722c7d94c87bac4eee89e13b1607
SortingFarmer <bengemrafdavid@gmail.com>
2024-12-17 18:44:16 +0100

    new database and some sort of base products layout

--- src/app/routed/products/products.component.ts
+++ src/app/routed/products/products.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../static/header/header.component";
+import { FooterComponent } from "../../static/footer/footer.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
