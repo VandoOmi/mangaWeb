48968884aa80a1b74e695a6a1d65aa04d2cf0d1a
vando02800 <vando.02800@gmail.com>
2024-12-19 10:39:06 +0100

    mangaBox

---
+++ src/app/routed/products/manga-box/manga-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manga-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './manga-box.component.html',
+  styleUrl: './manga-box.component.scss'
+})
+export class MangaBoxComponent {
+  @()
+}
--- src/app/routed/products/products.component.ts
+++ src/app/routed/products/products.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../static/header/header.component";
 import { FooterComponent } from "../../static/footer/footer.component";
+import { MangaBoxComponent } from "./manga-box/manga-box.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MangaBoxComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
