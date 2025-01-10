1f5d68390389dac6604aac6892a255966e35434e
fabi7798 <hoffmann.fabian16@gmail.com>
2025-01-10 11:57:50 +0100

    teams fertig

--- src/app/routed/team/team.component.ts
+++ src/app/routed/team/team.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { WerWirSindComponent } from "./wer-wir-sind/wer-wir-sind.component";
+import { WasWirTuenComponent } from "./was-wir-tuen/was-wir-tuen.component";
+import { UnsereWerteComponent } from "./unsere-werte/unsere-werte.component";
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [WerWirSindComponent],
+  imports: [WerWirSindComponent, WasWirTuenComponent, UnsereWerteComponent],
   templateUrl: './team.component.html',
   styleUrl: './team.component.scss'
 })
