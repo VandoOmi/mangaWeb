5d03c49d44ac84f75c0313fbca8f3f3eec2a1ef7
vando02800 <vando.02800@gmail.com>
2025-01-16 10:23:26 +0100

    idk auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, User } from '@angular/fire/auth';
-import { collectionData, doc, Firestore, addDoc, collection, deleteDoc, setDoc, getDoc, getDocs } from '@angular/fire/firestore';
-import { BehaviorSubject, Observable, catchError, from, map, mapTo, throwError} from 'rxjs';
+import { doc, Firestore, collection, deleteDoc, setDoc, getDoc, getDocs } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable, catchError, from, map, throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -51,7 +51,7 @@ export class AuthService {
     );
   }
 
-  addUserRole(uid: string, role: string): Observable<void> {
+  addUserRole(uid: string, email: string, role: string): Observable<void> {
     const userRoleToCreate = {uid,role};
     const docRef = doc(this.firestore, 'user-role', uid);
     const promise = setDoc(docRef, userRoleToCreate);
