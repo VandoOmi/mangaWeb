547a29c96a7315d71bc415682eb5f8019fc25004
fabi7798 <hoffmann.fabian16@gmail.com>
2025-01-16 10:41:39 +0100

    idk

--- src/app/routed/products/manga-box/manga-box.component.ts
+++ src/app/routed/products/manga-box/manga-box.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, NgModule } from '@angular/core';
 import { Manga_Dex } from '../../../../services/manga.service';
-import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-manga-box',
+  standalone: true,
+  imports: [NgFor],
   templateUrl: './manga-box.component.html',
   styleUrls: ['./manga-box.component.scss']
 })
@@ -11,10 +13,3 @@ export class MangaBoxComponent {
   @Input() manga!: Manga_Dex;
   @Input() selectedStyle: string = 'list';
 }
-
-@NgModule({
-  declarations: [MangaBoxComponent],
-  imports: [CommonModule],
-  exports: [MangaBoxComponent]
-})
-export class MangaBoxModule {}
