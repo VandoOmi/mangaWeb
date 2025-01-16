6f7479ba979254d798465b50d7df614357d29472
vando02800 <vando.02800@gmail.com>
2025-01-16 10:40:34 +0100

    manga service

--- src/services/manga.service.ts
+++ src/services/manga.service.ts
@@ -1,12 +1,65 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { CsvService } from './csv.service';
-import { Observable } from 'rxjs';
+import { Observable, elementAt, from, map } from 'rxjs';
+import { Firestore, doc, getDoc, collection, query, setDoc, where, docSnapshots, deleteDoc, getDocs } from '@angular/fire/firestore';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class MangaService {
+    db =  inject(Firestore);
+
+    createManga_Db(manga: Manga_DB): Observable<void> {
+        const mangaReference = doc(this.db, "manga-data", );
+        const promise = setDoc(mangaReference, manga);
+
+        return from(promise);
+    }
+
+    getManga_DbById(manga_id: string): Observable<Manga_DB> {
+        const document = doc(this.db, "manga-data",manga_id);
+        const promise = getDoc(document);
+
+        return from(promise).pipe(
+            map((docSnapshot) => {
+                if (docSnapshot.exists()) {
+                    const data = docSnapshot.data();
+                    return data as Manga_DB;
+                } else {
+                    throw new Error('Document does not exist');
+                }
+            })
+        )
+    }
+
+    getManga_DbByIds(): Observable<Manga_DB[]> {
+        const mangaDatabase = collection(this.db, "manga-data");
+        const promise = getDocs(mangaDatabase);
+
+        return from(promise).pipe(
+            map(
+                doc => doc.docs.map(
+                    doc => doc.data() as Manga_DB
+                ) as Manga_DB[]
+            )
+        )
+    }
+
+    updateManga(manga: Manga_DB): Observable<void>  {
+        const mangaRef = doc(this.db, "manga-data", manga.manga_id);
+        const promise = setDoc(mangaRef, manga); 
+
+        return from(promise);
+    }
+
+    deleteManga(manga_id: string): Observable<void> {
+            const mangaRef = doc(this.db, "manga-data", manga_id);
+            const  promise = deleteDoc(mangaRef);
+
+            return from(promise)
+    };
+
     //getManga_DexById(id: string): Observable<Manga_Dex>
 
     //getManga_DexByIds(id: string[]): Observable<Manga_Dex[]> 
@@ -26,7 +79,7 @@ export class MangaService {
 
 export interface Manga_DB {
     manga_id: string;
-    title_de: string;
+    title: string;
     cover_url: string;
 };
 
