75c00897d6ed840dfbe4751807e3fc185c09f22f
SortingFarmer <bengemrafdavid@gmail.com>
2025-01-16 10:14:38 +0100

    Enhance DetailMangaComponent template with conditional rendering for titles and demographics

--- src/app/routed/detail-manga/detail-manga.component.ts
+++ src/app/routed/detail-manga/detail-manga.component.ts
@@ -1,14 +1,15 @@
 import { Component, Injectable, Input } from '@angular/core';
 import { Manga } from '../../../services/manga.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-detail-manga',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './detail-manga.component.html',
   styleUrl: './detail-manga.component.scss'
 })
 export class DetailMangaComponent {
   @Input() manga!: Manga;
+  demographics!: ["shounen", "shoujo", "josei", "seinen"];
 }
